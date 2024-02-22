cdb149e21bf9acb82268bf38932cbfa64046d9ed
yamiranea <yamilarenea@gmail.com>
2024-02-22 11:21:21 +0100

    Creating card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Card as ProductCard } from "flowbite-react";
 
-function Card() {
-    return (
-        <div>Card</div>
-    )
+function Card({ imgSrc, title, subtitle, price, buttonText }) {
+  return (
+    <ProductCard className="max-w-sm" imgSrc={imgSrc} horizontal>
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">{subtitle}</p>
+      <p className="font-normal text-gray-700 dark:text-gray-400">{price}</p>
+      <button className="btn btn-primary">{buttonText}</button>
+    </ProductCard>
+  );
 }
 
-export default Card
+export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
 
 function HomePage() {
   return (
-    < >
-    <div>Hi, HomePage</div>
-    <h1>Explorar</h1>
+    <>
+      <div>Hi, HomePage</div>
+      <h1>Explorar</h1>
+      <div>
+        <Card
+          imgSrc="/images/blog/image-4.jpg"
+          title="Noteworthy technology acquisitions 2021"
+          subtitle="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
+          price="$100"
+          buttonText="Buy Now"
+        />
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
