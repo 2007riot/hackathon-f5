5c6552f9c0d5782f9a32c9520d53b0f14f1663b0
yamiranea <yamilarenea@gmail.com>
2024-02-22 20:25:18 +0100

    cards done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,25 +6,23 @@ import { Button } from "flowbite-react";
 function Card({ image, title, description, activities }) {
   return (
     <ProductCard className="w-3/12 bg-black rounded-xl m-2">
+      <div>
+        <img src={image} alt={title} className="rounded-lg w-auto" />
+      </div>
       <div class="flex flex-col">
-        <div>
-          <img src={image} alt={title} className="rounded-lg w-24 h-24" />
-        </div>
-        <div>
-          <h5 className="text-3xl font-bold tracking-tight text-white mt-4 mb-2">
-            {title}
-          </h5>
-          <p className="font-normal text-gray-400 dark:text-gray-400">
-            {description}
-          </p>
-          <div>
-            {activities &&
-              activities.map((activity) => (
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  {activity}
-                </span>
-              ))}
-          </div>
+        <h5 className="text-2xl font-bold tracking-tight text-white mt-4 mb-2">
+          {title}
+        </h5>
+        <p className="font-normal text-gray-400 dark:text-gray-400">
+          {description}
+        </p>
+        <div className="flex flex-row flex-wrap">
+          {activities &&
+            activities.map((activity) => (
+              <span className="inline-block bg-indigo-950 rounded-lg px-3 py-1 text-xs font-semibold text-white mr-2 mb-2">
+                {activity}
+              </span>
+            ))}
         </div>
       </div>
       <Button
