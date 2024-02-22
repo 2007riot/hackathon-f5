0ce34c27b3294533329f38c132abcfa1494ddd41
Leandra <leamontoya19@gmail.com>
2024-02-22 14:55:32 +0100

    I changed navbar for Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import Header from '../components/Navbar'
+import Header from '../components/Header'
 
 
 function HomePage() {
   return (
-    < >
+    <>
     <Header/>
-    
-    <div>Hi, HomePage</div>
-    <h1>Explorar</h1>
-    </>
-    
+    </>  
   )
 }
-
 export default HomePage
