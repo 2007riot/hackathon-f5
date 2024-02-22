2f337bcdc1ac76d38aea2d62cfbc2ec9b71b190a
RuthDanielaAguirre <argdiee@icloud.com>
2024-02-22 13:39:38 +0100

    footer done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
+
 import React from 'react'
+import  { AppFooter } from '../components/Footer'
 
 function HomePage() {
   return (
-    < >
+    <>
     <div>Hi, HomePage</div>
     <h1>Explorar</h1>
+    <AppFooter/>
     </>
     
   )
